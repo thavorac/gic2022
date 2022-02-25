7565dd326120689f3ef8fca30be0657b5b952f5a
Thavorac <thavorac.chun@gmail.com>
2022-02-25 11:42:47 +0700

    Convert plain HTML/CSS to VueJs app

---
+++ src/App.vue
@@ -0,0 +1,175 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <table>
+        <tr>
+          <td colspan="5">
+            <div id="screen">
+              <span id="screen_top">M=0</span>
+              <div id="screen_bottom">
+                <span id="operand1">0</span>
+                <span id="operator"></span>
+                <span id="operand2"></span>
+              </div>
+              <!-- <span id="screen_bottom">0</span> -->
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn btn-warning">MC</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning">MR</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning">M-</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-warning">M+</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn btn-light">7</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">8</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">9</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-secondary">÷</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">+/-</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn btn-light">4</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">5</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">6</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-secondary">x</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-secondary">-</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn btn-light">1</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">2</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">3</button>
+          </td>
+          <td rowspan="2">
+            <button type="button" class="btn btn-secondary long-btn">+</button>
+          </td>
+          <td rowspan="2">
+            <button type="button" class="btn btn-primary long-btn">=</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn btn-danger">C</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">0</button>
+          </td>
+          <td>
+            <button type="button" class="btn btn-light">.</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="alert alert-danger" id="message_panel" role="alert">
+      something wrong here
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container {
+  margin-top: 10em;
+  width: 300px;
+  border: 1px solid black;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+table {
+  border-spacing: 7px;
+  border-collapse: separate;
+}
+#screen {
+  border: 1px solid black;
+  padding: 7px;
+  width: 100%;
+  height: 4em;
+}
+#screen_top {
+  display: block;
+  font-size: 0.8rem;
+}
+#screen_bottom {
+  font-size: 1.8rem;
+  display: block;
+  text-align: right;
+}
+#operand2 {
+  background-color: skyblue;
+}
+#operator {
+  background-color: rosybrown;
+}
+.button-row {
+  display: flex;
+  justify-content: space-between;
+}
+button {
+  width: 45px;
+}
+.long-btn {
+  display: inline-block;
+  height: 80px;
+}
+
+/* Message panel */
+#message_panel {
+  width: 300px;
+  margin-top: 1em;
+  display: none;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
