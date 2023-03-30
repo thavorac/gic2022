0d3de3879b74d7350193d31249f9ad6c8f479c00
Thavorac <thavorac@sala.co>
2023-03-30 09:00:10 +0700

    Life cycle

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>Users</div>
+  <!-- TODO: Update user list -->
+  <div v-if="users && users.length > 0">
+    <UserListItem v-for="user in users" :user="user"></UserListItem>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import UserListItem from "./components/UserListItem.vue";
+export default {
+  data() {
+    return {
+      // now we don't have
+      users: [],
+    };
+  },
+  components: {
+    UserListItem,
+  },
+  beforeCreate() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        // handle success
+        this.users = response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  },
+};
+</script>
---
+++ src/components/UserListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{ user.name }}</h3>
+    <span>{{ renderedAddress }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserListItem",
+  props: ["user"],
+  computed: {
+    renderedAddress: function () {
+      if (this.user) {
+        const address =
+          this.user.address.suite +
+          ", " +
+          this.user.address.street +
+          ", " +
+          this.user.address.city +
+          ", " +
+          this.user.address.zipcode;
+        return address;
+      } else {
+        return "...";
+      }
+    },
+  },
+};
+</script>
