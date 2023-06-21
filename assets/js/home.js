acb40eb5f0c9a015e38b510390e3facced31fcf8
Thavorac <thavorac@sala.co>
2023-06-21 08:24:26 +0700

    Update asset game

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,33 +1,32 @@
-function drawImage(context, image) {
-  const imageTag = new Image(); // <img />
-  imageTag.src = image.src; // <img src="./assets/images/car.png" />
-
-  imageTag.onload = function () {
-    context.drawImage(imageTag, image.x, image.y);
-  };
-}
-
 const canvas = document.querySelector("#my-canvas");
 if (canvas.getContext) {
   let context = canvas.getContext("2d");
 
-  const car = {
-    src: "./assets/images/car.png",
-    x: 168,
-    y: 430,
+  let speed = 5;
+  const playGround = {
+    w: 1020,
+    h: 1243,
   };
-  const truck = {
-    src: "./assets/images/truck.png",
-    x: 240,
-    y: 40,
-  };
-  const stone = {
-    src: "./assets/images/stone.png",
-    x: 50,
-    y: 60,
-  };
+  let currentY = 0;
+  let current1Y = -playGround.h;
 
-  drawImage(context, car);
-  drawImage(context, truck);
-  drawImage(context, stone);
+  const road = new Image();
+  road.src = "./assets/images/road.png";
+
+  function startGame() {
+    window.requestAnimationFrame(startGame);
+
+    context.clearRect(0, 0, playGround.w, playGround.h);
+    context.drawImage(road, 0, current1Y);
+    context.drawImage(road, 0, currentY);
+    currentY += speed;
+    current1Y += speed;
+
+    if (currentY >= playGround.h) currentY = 0;
+    if (current1Y >= 0) current1Y = -playGround.h;
+  }
+
+  road.onload = () => {
+    startGame();
+  };
 }
