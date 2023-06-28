c96a0846c14023bfb37ee8e4c8d70e6e6e2c3bd1
Thavorac <thavorac@sala.co>
2023-06-28 09:00:19 +0700

    Update game

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,25 @@
+const defaultSpeed = 5;
+const playGround = {
+  w: 1020,
+  h: 1243,
+};
+
 class Player {
   constructor(name, car) {
     this.name = name;
     this.car = car;
     this.score = 0;
     this.level = 2;
+    this.x = 500;
+    this.y = 1000;
+  }
+}
+class Obstacle {
+  constructor(name, obstacle) {
+    this.name = name;
+    this.obstacle = obstacle;
+    this.x = Math.floor(Math.random() * playGround.w) + 1;
+    this.y = 0;
   }
 }
 
@@ -12,11 +28,6 @@ if (canvas.getContext) {
   let context = canvas.getContext("2d");
 
   // Variables
-  const defaultSpeed = 5;
-  const playGround = {
-    w: 1020,
-    h: 1243,
-  };
   let currentY = 0;
   let current1Y = -playGround.h;
 
@@ -26,8 +37,33 @@ if (canvas.getContext) {
   car.src = "./assets/images/red-car.png";
 
   const player1 = new Player("Sok", car);
+  const obstacles = new Array();
   let speed = player1.level * defaultSpeed;
 
+  for (let i = 0; i < 10; i++) {
+    obstacles.push(new Obstacle("Obstacle" + i, "test"));
+  }
+
+  console.log(obstacles);
+
+  // Add event listener to the game
+  document.addEventListener("keydown", function (e) {
+    // keydown (arrow left/right)
+    if (e.keyCode == 39) {
+      player1.x = player1.x + speed;
+      player1.x = player1.x > playGround.w ? playGround.w : player1.x;
+    }
+    if (e.keyCode == 37) {
+      player1.x = player1.x - speed;
+      player1.x = player1.x < 0 ? 0 : player1.x;
+    }
+  });
+  document.addEventListener("mousedown", function (e) {
+    let rect = canvas.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    console.log(x, y);
+  });
   // Functions
   function startGame() {
     window.requestAnimationFrame(startGame);
@@ -41,7 +77,7 @@ if (canvas.getContext) {
     if (currentY >= playGround.h) currentY = 0;
     if (current1Y >= 0) current1Y = -playGround.h;
 
-    context.drawImage(player1.car, 300, 1000);
+    context.drawImage(player1.car, player1.x, player1.y);
   }
 
   // Promises
